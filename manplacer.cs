e76c03f4088fdcd8568281976b077d44bc282360
Horizon <84783750+HorizonHacks@users.noreply.github.com>
2023-12-10 09:56:29 -0500

    Create manplacer.cs

---
+++ manplacer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class man_placer : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(117.4f, 0f, 23.1f);
+    }
+}
